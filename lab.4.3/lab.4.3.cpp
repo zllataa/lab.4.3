070642ae36329c6a644175342fc16589034dbb77
Zlata Polovka <91230475+zllataa@users.noreply.github.com>
2021-10-21 03:08:47 +0300

    Add files via upload

---
+++ lab.4.3/lab.4.3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "F" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (c < 0 && b != 0)
+			F = a * x * x + b * b * x;
+		else
+			if (c > 0 && b == 0)
+				F = (x + a) / (x + c);
+			else
+				F = x / c;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
